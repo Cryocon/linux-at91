5d2e4044ddf2d7078b4bd8c325c856bd65c8341a
Nicholas Walton <nicholas.walton@gmail.com>
2013-05-31 16:08:05 -0700

    Change dummy SPI0.0 CS from in-use PD17 to unused PD7.

--- arch/arm/mach-at91/board-m18.c
+++ arch/arm/mach-at91/board-m18.c
@@ -80,7 +80,7 @@ static struct spi_board_info m18_spi_devices[] = {
 		.max_speed_hz	= 1000 * 1000,
 		.mode		= SPI_MODE_0,
 		.irq            = -1,
-		.controller_data = AT91_PIN_PD17,
+		.controller_data = AT91_PIN_PD7,
 	},
 	{
 		.bus_num	= 1,
