4f96c3bbe3726cf762aee0062fcb2d76e2935df7
Nicholas Walton <nicholas.walton@gmail.com>
2013-05-31 15:51:02 -0700

    Change clock phase of spidev1.0 to match existing M18C.
    This seems to contradict the LCD module datasheet, but it works.

--- arch/arm/mach-at91/board-m18.c
+++ arch/arm/mach-at91/board-m18.c
@@ -87,7 +87,7 @@ static struct spi_board_info m18_spi_devices[] = {
 		.chip_select	= 0,
 		.modalias	= "spidev",
 		.max_speed_hz	= 3 * 1000 * 1000,
-		.mode		= SPI_MODE_0,
+		.mode		= SPI_MODE_1,
 		.irq            = -1,
 	},
 };
